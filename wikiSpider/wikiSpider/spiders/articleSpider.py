47ccd8f0302bb33e2322c404229d8fd8ad641dfc
zhe <1655604975@qq.com>
2018-08-08 10:10:58 +0800

    Python3中,scrapy使用LinkExtractor代替SgmlLinkExtractor 方法: from scrapy.linkextractors import LinkExtractor

--- wikiSpider/wikiSpider/spiders/articleSpider.py
+++ wikiSpider/wikiSpider/spiders/articleSpider.py
@@ -5,19 +5,21 @@
 # @FileName : articleSpider.py
 # @Project  : PyCharm
 
-from scrapy.selector import Selector
-from scrapy import Spider
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from wikiSpider.items import Article
 
-class ArticleSpider(Spider):
+
+class ArticleSpider(CrawlSpider):
     name = 'article'
-    allowed_domains = ['en.wikipedie.org']
-    start_urls = ['http://en.wikipedia.org/wiki/Main_Page',
-                  'http://en.wikipedia.org/wiki/Python_%28programming_language%29']
-    def parse(self, response):
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['http://en.wikipedia.org/wiki/Python_%28programming_language%29']
+    rules = [Rule(LinkExtractor(allow=('(/wiki/)((?!:).)*$'), ),
+                  callback='parse_item', follow=True)]
+
+    def parse_item(self, response):
         item = Article()
         title = response.xpath('//h1/text()')[0].extract()
         print('Title: ', title)
         item['title'] = title
         return item
-
