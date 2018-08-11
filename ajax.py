8307cfca93e0f253784e4b7cf64734fa41febb75
zhe <1655604975@qq.com>
2018-08-11 10:45:16 +0800

    通过检测元素的变换判断js执行是否完成

--- ajax.py
+++ ajax.py
@@ -6,20 +6,20 @@
 # @Project  : PyCharm
 
 from selenium import webdriver
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 
 BASE_DIR = os.path.dirname(__file__)
-phantomJS_path = os.path.join(BASE_DIR, 'phantomjs-2.1.1-windows', 'bin',
-                              'phantomjs.exe')
+
 chromedriver_path = os.path.join(BASE_DIR, 'chromedriver.exe')
 
-# driver = webdriver.PhantomJS(executable_path=phantomJS_path)
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
-# print(driver.page_source)
-print(driver.find_element_by_id('content').text)
-time.sleep(4)
-# print(driver.page_source)
-print(driver.find_element_by_id('content').text)
-driver.close()
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'loadedButton')))
+finally:
+    print(driver.find_element_by_id('content').text)
+    driver.close()
